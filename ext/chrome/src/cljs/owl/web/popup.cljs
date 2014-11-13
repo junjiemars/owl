6325483d85ae704d53cac8df2783981a72db7ff2
Junjie Mars <junjiemars@gmail.com>
2014-11-13 13:53:21 +0800

    popup is works

--- ext/chrome/src/cljs/owl/web/popup.cljs
+++ ext/chrome/src/cljs/owl/web/popup.cljs
@@ -2,11 +2,8 @@
   (:use [domina :only [by-id value set-value! set-attr!]])
   (:require [domina.events :as ev]))
 
-(defn set-options-link! [uri]
-  (let [u uri]
-    (set-attr! (by-id "options_link")
-               :href
-               (.getURL js/chrome.runtime u))))
+(defn set-link! [id uri]
+  (set-attr! (by-id id) :href (.getURL js/chrome.runtime uri)))
 
 (defn set-proxy-uri! [uri]
   (let [u uri
@@ -19,6 +16,11 @@
     (.log js/console e)
     (.log js/console (.getBackgroundPage js/chrome.extension))))
 
+(defn on-proxy-run []
+  (.log js/console "begin run proxy")
+  (.getSelected js/chrome.tabs (fn [t] (.log js/console t.url))))
+                                 
+
 (defn on-doc-ready
   []
   (let [ready-state (.-readyState js/document)]
@@ -26,9 +28,12 @@
              (by-id "popup"))
       (do 
           (.log js/console "#popup:on-doc-ready")
-          (set-options-link! "resources/public/options.html")
+          ;(set-options-link! "resources/public/options.html")
+          (set-link! "options_link" "resources/public/options.html")
+          (set-link! "echo_link" "resources/public/echo.html")
           (set-proxy-uri! "http://localhost:9001")
           (ev/listen! (by-id "proxy_uri") :onchange on-proxy-uri-change!)
+          (ev/listen! (by-id "proxy_run") :click on-proxy-run)
         true)
       false)))
 
