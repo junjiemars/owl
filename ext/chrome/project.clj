6f476e01d9f8aff6f3d35ecff9f96055627c77cf
南山竹 <junjiemars@gmail.com>
2016-04-23 18:26:35 +0800

    upgrade clojurescript

--- ext/chrome/project.clj
+++ ext/chrome/project.clj
@@ -2,15 +2,14 @@
   :description "Owl web front"
   :source-paths ["src/clj" "src/cljs" "src/brepl"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "0.0-2816"
+                 [org.clojure/clojurescript "1.8.40"
                   :exclusions [org.apache.ant/ant]]
-                 [compojure "1.2.1"]
-                 [ring/ring-defaults "0.1.3"]
+                 [compojure "1.5.0"]
+                 [ring/ring-defaults "0.2.0"]
                  [hiccup "1.0.5"]
-                 [domina "1.0.3"]
-                 ]
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-ring "0.9.0" #_ ("0.8.13")
+                 [domina "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-ring "0.9.7"
              :exclusions [org.clojure/clojure]]
             [lein-packer "0.1.0"]]
   :hooks [leiningen.cljsbuild]
@@ -32,10 +31,10 @@
                                  }]}
                    :pack {:mapping [{:source-paths ["manifest.json"
                                                     "resources/public"]
-                                     :target-path "target/packed"
+                                     :target-path "target/dev"
                                      :excludes [#"\w+\.\w+\~"]}]
                           :target {:type "crx"
-                                   :path "target/"}}}
+                                   :path "target/dev"}}}
              :pro {:cljsbuild {:builds
                                [{:source-paths ["src/cljs"]
                                  :compiler {:externs
@@ -47,7 +46,7 @@
                                             :warnings true}}]}
                    :pack {:mapping [{:source-paths ["manifest.json"
                                                     "resources/public"]
-                                     :target-path "target/packed"
+                                     :target-path "target/pro"
                                      :excludes [#"\w+\.\w+\~"]}]
                           :target {:type "crx"
-                                   :path "target/packed"}}}})
+                                   :path "target/pro"}}}})
